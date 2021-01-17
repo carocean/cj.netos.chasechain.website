d0707e107b0374a6686395581b973acb347aca01
cj <cj@ss.com>
2021-01-18 03:58:12 +0800

    1.0.0

--- cj.netos.chasechain.website.program/build/resources/main/site/web/js/viewer.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-    $('.my-gallery share').click(function(){
-        window.location.href=$(this).find('a').attr('href');
-    });
-    $('.demo p.fx_content').click(function(){
-        $(this).attr('style',"overflow:auto;display:block;");
-    });
-});
